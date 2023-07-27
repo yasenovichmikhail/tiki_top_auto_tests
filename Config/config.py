00ce333d2f07887cf7d7cd8ec22077784c577ec4
unknown <yasenovich.mikhail@mail.ru>
2023-07-27 16:35:16 +0300

    remove Config from repository

--- Config/config.py
+++
@@ -1,7 +0,0 @@
-class TestData:
-    CHROME_EXECUTABLE_PATH = "C:\chromedriver"
-    FIREFOX_EXECUTABLE_PATH = "C:\geckodriver"
-
-    BASE_URL = "https://tikitop.io/"
-    USER_NAME = "bobbyngayer@gmail.com"
-    PASSWORD = "qwertyasd"
