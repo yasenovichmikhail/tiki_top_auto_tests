009eab15cf4dcd2a28d010c766a1191a2d0ac445
unknown <yasenovich.mikhail@mail.ru>
2023-07-27 21:04:53 +0300

    created methods for login page

---
+++ Pages/LoginPage.py
@@ -0,0 +1,45 @@
+from selenium.webdriver.common.by import By
+
+from Pages.BasePage import BasePage
+
+
+class LoginPage(BasePage):
+
+    """Locators"""
+    SIGNUP_BUTTON = (By.XPATH, "//button[@class='CustomButton_btn__22u2s/"
+                               " CustomButton_colorWhite__MeJq0 CustomButton_btnSignUp__1ijqK']")
+
+    LOGIN_TAB = (By.XPATH, "//div[@class='AuthorizationTypeItem_item__2IwVY'][2]")
+
+    INPUT_EMAIL = (By.ID, "authorizationEmail")
+
+    INPUT_PASSWORD = (By.ID, "authorizationPassword")
+
+    LOGIN_BUTTON = (By.XPATH, "//button[@class='CustomButton_btn__22u2s/"
+                              " CustomButton_colorAqua__TKZR6 CustomButton_typeAuth__m__ns']")
+
+    """Constructor of the page class"""
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    """Page Actions"""
+
+    """It's used to get the page title"""
+    def get_login_page_title(self, title):
+        return self.get_title(title)
+
+    """It's used to check sign up button"""
+    def is_signup_button_exist(self):
+        return self.is_visible(self.SIGNUP_BUTTON)
+
+    """It's used to login to app"""
+    def login(self, username, password):
+        self.click(self.SIGNUP_BUTTON)
+        self.send_keys(self.INPUT_EMAIL, username)
+        self.send_keys(self.INPUT_PASSWORD, password)
+        self.click(self.LOGIN_BUTTON)
+
+
+
+
+
