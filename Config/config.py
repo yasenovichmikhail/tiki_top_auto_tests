c0addaadb8fc4709c6190af4c25a1cbcd78c8d43
unknown <yasenovich.mikhail@mail.ru>
2023-07-27 16:26:53 +0300

    created config file for tiki top project

---
+++ Config/config.py
@@ -0,0 +1,7 @@
+class TestData:
+    CHROME_EXECUTABLE_PATH = "C:\chromedriver"
+    FIREFOX_EXECUTABLE_PATH = "C:\geckodriver"
+
+    BASE_URL = "https://tikitop.io/"
+    USER_NAME = "bobbyngayer@gmail.com"
+    PASSWORD = "qwertyasd"
