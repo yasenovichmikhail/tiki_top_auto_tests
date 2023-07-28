8d5e67fff80ebc53cb726a9c53064316d415c770
user <yasenovich.mikhail@mail.ru>
2023-07-28 13:07:53 +0300

    modified config and conftest files

--- Config/config.py
+++ Config/config.py
@@ -5,3 +5,5 @@ class TestData:
     BASE_URL = "https://tikitop.io/"
     USER_NAME = "bobbyngayer@gmail.com"
     PASSWORD = "qwertyasd"
+
+    LOGIN_PAGE_TITLE = "Home | TikiTop"
--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,15 +1,14 @@
 from selenium import webdriver
 import pytest
-import
-
 from Config.config import TestData
 
-@pytest.fixture(params=["chrome", "firefox"], scope="class")
+
+@pytest.fixture(params=["chrome"], scope="function")
 def init_driver(request):
     if request.param == "chrome":
-        web_driver = webdriver.Chrome(executable_path=TestData.CHROME_EXECUTABLE_PATH)
-    elif request.param == "firefox":
-        web_driver = webdriver.Firefox(executable_path=TestData.FIREFOX_EXECUTABLE_PATH)
+        web_driver = webdriver.Chrome()
+    elif request.param == "edge":
+        web_driver = webdriver.Edge()
     request.cls.driver = web_driver
     yield web_driver
     web_driver.quit()
