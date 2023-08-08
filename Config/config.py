5a34f6fbd7a84d300f1baca06649698f91d96347
user <yasenovich.mikhail@mail.ru>
2023-08-08 17:02:22 +0300

    modified class TestData

--- Config/config.py
+++ Config/config.py
@@ -4,4 +4,12 @@ class TestData:
     USER_NAME = "bobbyngayer@gmail.com"
     PASSWORD = "qwertyasd"
 
-    LOGIN_PAGE_TITLE = "Home | TikiTop"
+    HOME_PAGE_TITLE = "Home | TikiTop"
+    PRICING_PAGE_TITLE = "Pricing | TikiTop"
+    FAQ_PAGE_TITLE = "FAQ | TikiTop"
+    CONTACT_US_PAGE_TITLE = "Contact us | TikiTop"
+    PROFILE_PAGE_TITLE = "Account | TikiTop"
+    CHANGE_PASSWORD_PAGE_TITLE = "Change Password | TikiTop"
+    
+
+
