34aff7c3b9a44ffa6d3359b3568fa2af98ca5da0
user <yasenovich.mikhail@mail.ru>
2023-08-15 13:56:11 +0300

    created locators for the headers of the home page. Added header text validation

--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -11,6 +11,7 @@ class HomePage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
         self.driver.get(TestData.BASE_URL)
+
     """Page Actions"""
 
     def check_home_page_title(self):
@@ -20,24 +21,41 @@ class HomePage(BasePage):
     def is_boost_your_profile_container_presented(self):
         assert self.is_element_present(*HomePageLocators.BOOST_YOUR_TIKTOK_PROFILE_CONTAINER), "Boost your profile " \
                                                                                               "section is not presented"
+        actual_result = self.get_element_text(HomePageLocators.BOOST_YOUR_TIKTOK_PROFILE_HEADER)
+        expected_result = "BOOST YOUR TIKTOK PROFILE & VIDEOS"
+        assert actual_result == expected_result, f"expected {expected_result}, got {actual_result}"
 
     def is_what_do_we_offer_container_presented(self):
         assert self.is_element_present(*HomePageLocators.WHAT_DO_WE_OFFER_CONTAINER), "What do we offer " \
                                                                                               "section is not presented"
+        actual_result = self.get_element_text(HomePageLocators.WHAT_DO_WE_OFFER_HEADER)
+        expected_result = "WHAT DO WE OFFER?"
+        assert actual_result == expected_result, f"expected {expected_result}, got {actual_result}"
 
     def is_what_we_have_already_done_container_presented(self):
         assert self.is_element_present(*HomePageLocators.WHAT_WE_HAVE_ALREADY_DONE_CONTAINER), "What we have already done " \
                                                                                             "section is not presented"
+        actual_result = self.get_element_text(HomePageLocators.WHAT_WE_HAVE_ALREADY_DONE_HEADER)
+        expected_result = "WHAT WE'VE ALREADY DONE"
+        assert actual_result == expected_result, f"expected {expected_result}, got {actual_result}"
 
     def is_promotion_formats_container_presented(self):
         assert self.is_element_present(*HomePageLocators.PROMOTION_FORMATS_CONTAINER), "Promotion formats " \
                                                                                             "section is not presented"
+        actual_result = self.get_element_text(HomePageLocators.PROMOTION_FORMATS_HEADER)
+        expected_result = "PROMOTION FORMATS"
+        assert actual_result == expected_result, f"expected {expected_result}, got {actual_result}"
 
     def is_our_clients_trust_us_container_presented(self):
         assert self.is_element_present(*HomePageLocators.OUR_CLIENTS_TRUST_US_CONTAINER), "Promotion formats " \
                                                                                             "section is not presented"
+        actual_result = self.get_element_text(HomePageLocators.OUR_CLIENTS_TRUST_US_HEADER)
+        expected_result = "OUR CLIENTS TRUST US"
+        assert actual_result == expected_result, f"expected {expected_result}, got {actual_result}"
 
     def is_any_questions_container_presented(self):
         assert self.is_element_present(*HomePageLocators.ANY_QUESTIONS_CONTAINER), "Any questions " \
                                                                                             "section is not presented"
-
+        actual_result = self.get_element_text(HomePageLocators.ANY_QUESTIONS_HEADER)
+        expected_result = "ANY QUESTIONS?"
+        assert actual_result == expected_result, f"expected {expected_result}, got {actual_result}"
--- Pages/locators.py
+++ Pages/locators.py
@@ -17,11 +17,17 @@ class HomePageLocators:
     ANY_QUESTIONS_MORE_INFO_BUTTON = (By.XPATH, "//a[@class='LinkButton_link___jzSK LinkButton_colorBlack__WwvPN']")
     ANY_QUESTIONS_CONTACT_US_BUTTON = (By.XPATH, "//a[@class='LinkButton_link___jzSK LinkButton_colorWhiteWithBorder__zLVqH']")
     BOOST_YOUR_TIKTOK_PROFILE_CONTAINER = (By.XPATH, "//section[@class='container HeaderOffer_headerOffer__0Nk4c']")
+    BOOST_YOUR_TIKTOK_PROFILE_HEADER = (By.XPATH, "//h1[@class='HeaderOffer_title__XSSqh']")
     WHAT_DO_WE_OFFER_CONTAINER = (By.XPATH, "//section[@class='container'][1]")
+    WHAT_DO_WE_OFFER_HEADER = (By.XPATH, "//h2[@class='OurOffer_title__dM8j1']")
     WHAT_WE_HAVE_ALREADY_DONE_CONTAINER = (By.XPATH, "//section[@class='container DoneOrders_container__aI7tA']")
+    WHAT_WE_HAVE_ALREADY_DONE_HEADER = (By.XPATH, "//h2[@class='DoneOrders_title__StHjc']")
     PROMOTION_FORMATS_CONTAINER = (By.XPATH, "//section[@class='container'][2]")
+    PROMOTION_FORMATS_HEADER = (By.XPATH, "//h2[@class='PromotionFormats_title__fcjYc']")
     OUR_CLIENTS_TRUST_US_CONTAINER = (By.XPATH, "//section[@class='container OurClients_container__rlkMW']")
+    OUR_CLIENTS_TRUST_US_HEADER = (By.XPATH, "//h2[@class='OurClients_title__r45Yp']")
     ANY_QUESTIONS_CONTAINER = (By.XPATH, "//section[@class='container AnyQuestions_container__7229q']")
+    ANY_QUESTIONS_HEADER = (By.XPATH, "//h2[@class='AnyQuestions_title__7gIg6']")
 
 
 class PricingPageLocators:
