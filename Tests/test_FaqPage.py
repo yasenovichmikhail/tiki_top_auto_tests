a8c6af34c32f7341432cc5699931c2559096dd49
user <yasenovich.mikhail@mail.ru>
2023-08-23 13:56:32 +0300

    added test cases for Faq page

--- Tests/test_FaqPage.py
+++ Tests/test_FaqPage.py
@@ -36,7 +36,7 @@ class TestFaq(BaseTest):
         page.go_to_faq_page_from_header_link()
         page.is_get_free_views_visible()
 
-    def test_user_can_see_popular_questions_container(self):
+    def test_guest_can_see_popular_questions_container(self):
         page = FaqPage(self.driver)
         page.go_to_faq_page_from_header_link()
         page.is_popular_questions_container_presented()
@@ -46,10 +46,56 @@ class TestFaq(BaseTest):
         page.go_to_faq_page_from_header_link()
         page.is_footer_visible()
 
-    @pytest.mark.smoke
-    def test_guest_can_expand_first_question(self):
+    def test_guest_can_go_to_home_page_from_header_link_faq(self):
+        page = FaqPage(self.driver)
+        page.go_to_faq_page_from_header_link()
+        page.go
+
+    def test_guest_can_expand_collapse_first_question(self):
         page = FaqPage(self.driver)
         page.go_to_faq_page_from_header_link()
         page.expand_first_question()
+        page.collapse_first_question()
 
+    def test_guest_can_expand_collapse_second_question(self):
+        page = FaqPage(self.driver)
+        page.go_to_faq_page_from_header_link()
+        page.expand_second_question()
+        page.collapse_second_question()
+
+    def test_guest_can_expand_collapse_third_question(self):
+        page = FaqPage(self.driver)
+        page.go_to_faq_page_from_header_link()
+        page.expand_third_question()
+        page.collapse_third_question()
+
+    def test_guest_can_expand_collapse_fourth_question(self):
+        page = FaqPage(self.driver)
+        page.go_to_faq_page_from_header_link()
+        page.expand_fourth_question()
+        page.collapse_fourth_question()
+
+    def test_guest_can_expand_collapse_fifth_question(self):
+        page = FaqPage(self.driver)
+        page.go_to_faq_page_from_header_link()
+        page.expand_fifth_question()
+        page.collapse_fifth_question()
+
+    def test_guest_can_expand_collapse_sixth_question(self):
+        page = FaqPage(self.driver)
+        page.go_to_faq_page_from_header_link()
+        page.expand_sixth_question()
+        page.collapse_sixth_question()
+
+    def test_guest_can_expand_collapse_seventh_question(self):
+        page = FaqPage(self.driver)
+        page.go_to_faq_page_from_header_link()
+        page.expand_seventh_question()
+        page.collapse_seventh_question()
+
+    def test_guest_can_expand_collapse_eighth_question(self):
+        page = FaqPage(self.driver)
+        page.go_to_faq_page_from_header_link()
+        page.expand_eighth_question()
+        page.collapse_eighth_question()
 
