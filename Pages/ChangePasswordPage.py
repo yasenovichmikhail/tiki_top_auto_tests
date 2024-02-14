0b97e16d572c656d9f7ea61aa84f65ef9e7759ff
user <yasenovich.mikhail@mail.ru>
2024-02-14 15:55:36 +0300

    added base methods for ChangePasswordPage class

---
+++ Pages/ChangePasswordPage.py
@@ -0,0 +1,22 @@
+from Pages.BasePage import BasePage
+from Pages.locators import ChangePasswordLocators
+from Config.config import TestData
+
+
+class ChangePasswordPage(BasePage):
+
+    def set_old_password(self, value):
+        self.send_keys(*ChangePasswordLocators.OLD_PASSWORD_TEXT_FIELD, value)
+
+    def set_new_password(self, value):
+        self.send_keys(*ChangePasswordLocators.NEW_PASSWORD_TEXT_FIELD, value)
+
+    def confirm_new_password(self, value):
+        self.send_keys(*ChangePasswordLocators.CONFIRM_PASSWORD_TEXT_FIELD, value)
+
+    def click_change_button(self):
+        self.click(*ChangePasswordLocators.CHANGE_BUTTON)
+
+    def click_back_button(self):
+        self.click(*ChangePasswordLocators.GO_BACK_BUTTON)
+
