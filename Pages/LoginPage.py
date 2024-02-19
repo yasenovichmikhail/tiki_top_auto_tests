b3994a677c4f7c779c95ce5a8a853f7bcf5ea43d
user <yasenovich.mikhail@mail.ru>
2024-02-19 17:13:41 +0300

    added methods for Login Page object

--- Pages/LoginPage.py
+++ Pages/LoginPage.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium.webdriver.common.by import By
 
 from Config.config import TestData
@@ -16,8 +17,47 @@ class LoginPage(BasePage):
 
     """It's used to login to app"""
 
-    def login(self, username, password):
+    def open_login_form(self):
         self.click(BasePageLocators.SIGNUP_BUTTON)
-        self.send_keys(LoginPageLocators.INPUT_EMAIL, username)
+
+    def click_sign_up_tab(self):
+        self.click(LoginPageLocators.SIGNUP_TAB)
+
+    def click_login_tab(self):
+        self.click(LoginPageLocators.LOGIN_TAB)
+
+    def click_google_auth_button(self):
+        self.click(LoginPageLocators.GOOGLE_AUTH_BUTTON)
+
+    def click_facebook_auth_button(self):
+        self.click(LoginPageLocators.FACEBOOK_AUTH_BUTTON)
+
+    def set_email(self, email):
+        self.send_keys(LoginPageLocators.INPUT_EMAIL, email)
+
+    def set_password(self, password):
         self.send_keys(LoginPageLocators.INPUT_PASSWORD, password)
+
+    def click_forgot_password_button(self):
+        self.click(LoginPageLocators.FORGOT_PASSWORD_BUTTON)
+
+    def show_password(self):
+        self.click(LoginPageLocators.SHOW_PASSWORD_ICON)
+
+    def hide_password(self):
+        self.click(LoginPageLocators.HIDE_PASSWORD_ICON)
+
+    def click_create_my_account_button(self):
+        self.click(LoginPageLocators.CREATE_MY_ACCOUNT_BUTTON)
+
+    def click_login_button(self):
         self.click(LoginPageLocators.LOGIN_BUTTON)
+
+    def click_close_login_page(self):
+        self.click(LoginPageLocators.CLOSE_LOGIN_PAGE_BUTTON)
+
+    def login(self):
+        self.click_sign_up_tab()
+        self.set_email(TestData.USER_EMAIL)
+        self.set_password(TestData.PASSWORD)
+        self.click_create_my_account_button()
